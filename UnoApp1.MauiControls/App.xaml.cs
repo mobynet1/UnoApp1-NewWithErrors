b3dcf5f3a31d0041ad4d345e613413bcabcf30cb
Steve Miller <steve@mobynet.io>
2024-10-14 09:04:54 -0700

    Add project files.

---
+++ UnoApp1.MauiControls/App.xaml.cs
@@ -0,0 +1,9 @@
+namespace UnoApp1.MauiControls;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+}
---
+++ UnoApp1.MauiControls/EmbeddedControl.xaml.cs
@@ -0,0 +1,10 @@
+namespace UnoApp1.MauiControls;
+
+public partial class EmbeddedControl : ContentView
+{
+    public EmbeddedControl()
+    {
+        InitializeComponent();
+    }
+
+}
